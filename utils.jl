7b1e28e330dc8f1dca083d393200c11142c18b2e
Sam Boysel <sboysel@gmail.com>
2015-01-02 23:24:46 -0800

    rewritten error handling

--- utils.jl
+++ utils.jl
@@ -7,11 +7,6 @@ SHARED_SECRET = "WqeyUj33Q92QcKU4uklKlA"
 
 BASE_URL = "http://developer.echonest.com/api/v4/"
 
-#artist = split(readall("artistMethods"), "\n")
-#song = split(readall("songMethods"), "\n")
-#genre = split(readall("genreMethods"), "\n")
-#track = split(readall("trackMethods"), "\n")
-
 METHODS_DICT = Dict("artist" => ["biographies", "blogs", "familiarity",
                             "hotttnesss", "images", "list_genres",
                             "list_terms", "news", "profile", "reviews",
@@ -88,45 +83,33 @@ end
 # artist base method (query method, name, options dicitonary)
 function artist(method::String, name::String, options::Dict)
     q = buildQuery("artist", method, name, options)
-    resp = Requests.get(q)
-    if resp.status != 200
-        error("Bad response.  Make sure you are using appropriate parameters.
-        See http://developer.echonest.com/docs/v4/artist.html")
-    end
-    return JSON.parse(IOBuffer(resp.data))["response"]
+    r = JSON.parse(IOBuffer(Requests.get(q).data))["response"]
+    println(r["status"]["message"])
+    return r
 end
 
 # artist method for passing only a query method and name (no options).
 function artist(method::String, name::String)
     q = buildQuery("artist", method, name)
-    resp = Requests.get(q)
-    if resp.status != 200
-        error("Bad response.  Make sure you are using appropriate parameters.
-        See http://developer.echonest.com/docs/v4/artist.html")
-    end
-    return JSON.parse(IOBuffer(resp.data))["response"]
+    r = JSON.parse(IOBuffer(Requests.get(q).data))["response"]
+    println(r["status"]["message"])
+    return r 
 end
 
 # artist method for passing only a query method (no options, no name).
 function artist(method::String)
     q = buildQuery("artist", method)
-    resp = Requests.get(q)
-    if resp.status != 200
-        error("Bad response.  Make sure you are using appropriate parameters.
-        See http://developer.echonest.com/docs/v4/artist.html")
-    end
-    return JSON.parse(IOBuffer(resp.data))["response"]
+    r = JSON.parse(IOBuffer(Requests.get(q).data))["response"]
+    println(r["status"]["message"])
+    return r 
 end
 
 # artist method for passing a method with options (no name).
 function artist(method::String, name::String, options::Dict)
     q = buildQuery("artist", method, options)
-    resp = Requests.get(q)
-    if resp.status != 200
-        error("Bad response.  Make sure you are using appropriate parameters.
-        See http://developer.echonest.com/docs/v4/artist.html")
-    end
-    return JSON.parse(IOBuffer(resp.data))["response"]
+    r = JSON.parse(IOBuffer(Requests.get(q).data))["response"]
+    println(r["status"]["message"])
+    return r 
 end
 
 function getartistid(name::String)
